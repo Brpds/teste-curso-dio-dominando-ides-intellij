18d2c02006e91e96bf6233f225e5e45f88ff3bd1
Brpds <brunnophoenix@gmail.com>
2022-01-27 20:36:06 -0300

    Commit 2 cria class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -5,8 +5,45 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro livro1 = new Livro("O problema dos 3 corpos", 380);
+        System.out.println(livro1);
+
         /*int a = 5;
         int b = 3;
         System.out.println("três mais cinco é : " + (a+b));*/
+    }
+}
+class Livro {
+    private String nome;
+    private Integer numPags;
+
+
+    public Livro(String nome, Integer numPags) {
+        this.nome = nome;
+        this.numPags = numPags;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPags() {
+        return numPags;
+    }
+
+    public void setNumPags(Integer numPags) {
+        this.numPags = numPags;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPags=" + numPags +
+                '}';
     }
 }
